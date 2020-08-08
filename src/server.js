534b7dd25b2eb866de2369d43403dec49db279be
cjatoba <claytonjatoba@gmail.com>
2020-08-08 14:55:56 -0300

    criacao de dados provisórios no server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,47 @@
+const proffys = [
+    {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4", 
+        whatsapp: "954785224", 
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum magnam eos distinctio, veritatis minus id quis quaerat officiis rerum neque nulla cupiditate laborum sapiente, facilis debitis eum tempore atque?", 
+        subject: "Química", 
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Clayton", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4", 
+        whatsapp: "954785224", 
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum magnam eos distinctio, veritatis minus id quis quaerat officiis rerum neque nulla cupiditate laborum sapiente, facilis debitis eum tempore atque?", 
+        subject: "Química", 
+        cost: "30",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
+
+function pageLanding(req, res) {
+    return res.sendFile(__dirname + "/views/index.html")
+}
+
+function pageStydy(req, res) {
+    return res.sendFile(__dirname + "/views/study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStydy)
+.get("/give-classes", pageGiveClasses)
 
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
 .listen(5500)
