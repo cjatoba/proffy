c2439063831d4c509158ddb2e49265d147d47b65
cjatoba <claytonjatoba@gmail.com>
2020-08-09 09:18:08 -0300

    Correção dos tipos de dados no banco

--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química", 
+        subject: 1, 
         cost: "20"        
     }
 
@@ -31,4 +31,30 @@ Database.then(async (db) => {
     ]
 
     //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    //Consultar os dados inseridos
+
+    //Todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    //Consultar as classes de um professor específico e trazer os dados dele
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectedClassesAndProffys)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    // console.log(selectClassesSchedules)
 })
