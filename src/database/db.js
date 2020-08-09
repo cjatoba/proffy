ae8b5607304c0201af8ca3bae4300f96bcd65812
cjatoba <claytonjatoba@gmail.com>
2020-08-08 22:55:47 -0300

    Exportando o db de db.js e importando no test.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,8 @@
 const DataBase = require('sqlite-async')
-DataBase.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
     //Criar as tabelas no banco
-    db.exec(`
+    return db.exec(`
         CREATE TABLE IF NOT EXISTS proffys (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
@@ -28,3 +27,5 @@ function execute(db) {
         );
     `)
 }
+
+module.exports = DataBase.open(__dirname + '/database.sqlite').then(execute)
