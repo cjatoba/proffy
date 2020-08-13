505ea64507b988db1cb580a64af97dfd32346c6b
cjsantos <claytonjatoba@gmail.com>
2020-08-12 23:33:44 -0300

    fim da aula

--- src/pages.js
+++ src/pages.js
@@ -36,6 +36,10 @@ async function pageStudy(req, res) {
         const db = await Database
         const proffys = await db.all(query)
 
+        proffys.map(() => {
+            proffys.subject= getSubject(proffys.subject)
+        })
+
         return res.render('study.html', { proffys, subjects, filters, weekdays })
     } catch (error) {
         console.log(error)
@@ -46,10 +50,45 @@ function pageGiveClasses(req, res) {
     return res.render("give-classes.html", { subjects, weekdays })
 }
 
-function saveClasses (req, res) {
+async function saveClasses (req, res) {
     const createProffy = require("./database/createProffy")
 
     const data = req.body; 
+
+    const proffyValue = {
+        name: re.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map(
+        (weekday, index) => {
+
+            return {
+                weekday,
+                time_from: convertHoursToMinutes(req.body.time_from[index]),
+                time_to: convertHoursToMinutes(req.body.time_to[index]),
+            }
+    })
+
+    try {
+        const db = await Database
+        await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        
+    }
 
     return res.redirect("/study")
 }
